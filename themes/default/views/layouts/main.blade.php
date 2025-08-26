1c8241d97ac351440e5ad28aa86a5e6686893a10
MrWeez <git@mrweez.dev>
2025-08-26 19:17:52 +0300

    refactor: :recycle: even more improved footer structure

--- themes/default/views/layouts/main.blade.php
+++ themes/default/views/layouts/main.blade.php
@@ -456,24 +456,34 @@
         @include('modals.redeem_voucher_modal')
     </div>
     <!-- /.content-wrapper -->
-    <footer class="main-footer">
-        Copyright &copy; {{ date('Y') }} <a href="{{ url('/') }}"><b>{{ config('app.name', "Ctrlpanel.gg") }}</b></a>.
-        All rights reserved.
-        Powered by <a href="https://CtrlPanel.gg"><b>CtrlPanel</b></a> &copy; 2021-{{ date('Y') }}.
-
-        {{-- Show imprint and privacy link --}}
-        <div class="float-right d-none d-sm-inline-block">
-            @if ($website_settings->show_imprint)
-                <a target="_blank" href="{{ route('terms', 'imprint') }}"><strong>{{ __('Imprint') }}</strong></a> |
-            @endif
-            @if ($website_settings->show_privacy)
-                <a target="_blank" href="{{ route('terms', 'privacy') }}"><strong>{{ __('Privacy') }}</strong></a>
-            @endif
-            @if ($website_settings->show_tos)
-                | <a target="_blank"
-                     href="{{ route('terms', 'tos') }}"><strong>{{ __('Terms of Service') }}</strong></a>
-            @endif
+    <footer class="main-footer d-flex justify-content-between">
+        {{-- Copyright section --}}
+        <div class="d-inline-block">
+            Copyright &copy; {{ date('Y') }} <a href="{{ url('/') }}"><b>{{ config('app.name', "Ctrlpanel.gg") }}</b></a>.
+            All rights reserved.
+            Powered by <a href="https://CtrlPanel.gg"><b>CtrlPanel</b></a> &copy; 2021-{{ date('Y') }}.
         </div>
+
+        @if ($website_settings->show_imprint || $website_settings->show_privacy || $website_settings->show_tos)
+            {{-- Show imprint and privacy link --}}
+            <div class="d-none d-sm-inline-block">
+                @if ($website_settings->show_imprint)
+                    <a target="_blank" href="{{ route('terms', 'imprint') }}"><strong>{{ __('Imprint') }}</strong></a>
+                @endif
+                @if ($website_settings->show_imprint && $website_settings->show_privacy || $website_settings->show_imprint && $website_settings->show_tos)
+                    |
+                @endif
+                @if ($website_settings->show_privacy)
+                    <a target="_blank" href="{{ route('terms', 'privacy') }}"><strong>{{ __('Privacy') }}</strong></a>
+                @endif
+                @if ($website_settings->show_privacy && $website_settings->show_tos)
+                    |
+                @endif
+                @if ($website_settings->show_tos)
+                    <a target="_blank" href="{{ route('terms', 'tos') }}"><strong>{{ __('Terms of Service') }}</strong></a>
+                @endif
+            </div>
+        @endif
     </footer>
 
     <!-- Control Sidebar -->
